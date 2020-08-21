6dbd3b0b8bb66899a518a345a10a228334c70f59
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-21 01:37:23 -0300

    Backend da Aplicação

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack.qhcfd.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
