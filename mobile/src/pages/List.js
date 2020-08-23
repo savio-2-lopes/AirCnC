4504d102a21087440df2638ee368ec57502ccf59
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-23 18:23:30 -0300

    Aplicação Mobile do projeto

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
 
-import SpotList from 	'../components/SpotList';
-import logo from 		'../assets/logo.png';
+import SpotList from '../components/SpotList';
+import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.1.102:3333', {
+        query: { user_id }
+      })
+
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+      })
+    })
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storagedTechs => {
