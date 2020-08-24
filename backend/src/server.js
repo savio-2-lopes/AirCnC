c0b34be73d7b50ee33a5f4f293fa3e8458617ff3
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:21:41 -0300

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,11 @@ const cors      = require('cors');
 const path      = require('path');
 const socketio  = require('socket.io');
 const http      = require('http');
+
 const routes    = require('./routes');
+
 const app       = express();
+
 const server    = http.Server(app);
 const io        = socketio(server);
 
@@ -13,11 +16,11 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack.qhcfd.mongodb.net/
     useNewUrlParser:    true,
     useUnifiedTopology: true,
 });
+
 const connectedUsers = {};
 
 io.on('connection', socket => {
   const { user_id } = socket.handshake.query;
-
   connectedUsers[user_id] = socket.id;
 });
 
@@ -26,10 +29,12 @@ app.use((req, res, next) => {
   req.connectedUsers = connectedUsers;
 
   return next();
-})
+});
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+
 server.listen(3333);
