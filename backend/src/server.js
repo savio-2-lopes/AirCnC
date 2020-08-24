020a470de17e95bcf849facdc9c0b7458ca22a14
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:18:43 -0300

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,18 @@
-const express       = require('express');
-const mongoose      = require('mongoose');
-const cors          = require('cors');
-const path          = require('path');
-
-const socketio      = require('socket.io');
-const http          = require('http');
-
-const routes        = require('./routes');
-
-const app           = express();
-const server        = http.Server(app);
-const io            = socketio(server);
+const express   = require('express');
+const mongoose  = require('mongoose');
+const cors      = require('cors');
+const path      = require('path');
+const socketio  = require('socket.io');
+const http      = require('http');
+const routes    = require('./routes');
+const app       = express();
+const server    = http.Server(app);
+const io        = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack.qhcfd.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser:    true,
     useUnifiedTopology: true,
 });
-
-
 const connectedUsers = {};
 
 io.on('connection', socket => {
@@ -34,11 +29,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
-
 app.use(routes);
-
 server.listen(3333);
