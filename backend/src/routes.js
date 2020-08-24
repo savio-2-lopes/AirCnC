5e39fb15b2a16dcb68742a9422c2dac5b2b33d16
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:16:16 -0300

    routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -24,31 +24,3 @@ routes.post('/bookings/:booking_id/approvals',  ApprovalController.store);
 routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
-
-
-/*const express               = require('express');
-const multer                = require('multer');
-const uploadConfig          = require('./config/upload');
-
-const SessionController     = require('./controllers/SessionController');
-const SpotController        = require('./controllers/SpotController');
-const DashboardController   = require('./controllers/DashboardController');
-const ApprovalController    = require('./controllers/ApprovalController');
-const RejectionController   = require('./controllers/RejectionController');
-const BookingController     = require('./controllers/BookingController');
-
-const routes                = express.Router();
-const upload                = multer(uploadConfig);
-
-routes.post ('/sessions',   SessionController.store);
-
-routes.get  ('/spots',      SpotController.index);
-routes.post ('/spots',      upload.single('thumbnail'), SpotController.store);
-
-routes.get ('/dashboard',   DashboardController.show);
-routes.post('/spots/:spot_id/bookings', BookingController.store);
-
-routes.get ('/bookings/:booking_id/approvals',   ApprovalController.store);
-routes.get ('/bookings/:booking_id/rejections',  RejectionController.show);
-
-module.exports = routes;*/
