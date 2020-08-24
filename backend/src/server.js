dc38cafcaae1a2bbfdcf1f2b725e6f7cc765ed7b
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:22:49 -0300

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,6 +21,7 @@ const connectedUsers = {};
 
 io.on('connection', socket => {
   const { user_id } = socket.handshake.query;
+    
   connectedUsers[user_id] = socket.id;
 });
 
@@ -29,7 +30,7 @@ app.use((req, res, next) => {
   req.connectedUsers = connectedUsers;
 
   return next();
-});
+})
 
 app.use(cors());
 app.use(express.json());
