2b8abb275c9e1bc5e36c6f8daa7f9ec4f06a38d9
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:07:30 -0300

    ApprovalController.js

--- backend/src/controllers/ApprovalController.js
+++ backend/src/controllers/ApprovalController.js
@@ -15,16 +15,3 @@ module.exports = {
         return res.json(booking);
     }
 };
-
-/*
-const Booking = require('../models/Booking');
-
-module.exports ={
-    async store(req, res) {
-        const { booking_id }    = req.params;
-        const booking           = await (await Booking.findById(booking_id)).populated('spot');
-        booking.approved        = true;
-        await booking.save();
-        return res.json(booking); 
-    }
-};*/
