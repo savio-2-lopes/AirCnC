09fe04c10e7df4cc28b271d27f7794a6400e31b8
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:09:28 -0300

    BookingController

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,8 +1,7 @@
 const Booking = require('../models/Booking');
 
 module.exports = {
-    async store(req, res)
-    {
+    async store(req, res){
         const { user_id } = req.headers;
         const { spot_id } = req.params;
         const { date    } = req.body;
