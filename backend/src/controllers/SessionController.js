0b88ce209713c8344e10252e7ecd6d24f0a5b78c
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:11:21 -0300

    SessionController

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,15 +1,12 @@
-//index, show, store, update, destroy
 const User = require('../models/User');
 
 module.exports = {
     async store(req, res){
         const {email} = req.body;
         let user = await User.findOne({email});
-        
         if (!user) {
             user = await User.create({ email });
         }
-        
         return res.json(user);
     }
 };
