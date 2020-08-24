ab1671e6e0bd746bb6632f0f3a902529cbd6aac5
Savio <60948849+savio-2-lopes@users.noreply.github.com>
2020-08-24 07:10:15 -0300

    RejectionController

--- backend/src/controllers/RejectionController.js
+++ backend/src/controllers/RejectionController.js
@@ -15,15 +15,3 @@ module.exports = {
     return res.json(booking);
   }
 };
-
-/*const Booking = require('../models/Booking');
-
-module.exports ={
-    async store(req, res) {
-        const { booking_id }    = req.params;
-        const booking           = await (await Booking.findById(booking_id)).populated('spot');
-        booking.approved        = false;
-        await booking.save();
-        return res.json(booking); 
-    }
-};*/
